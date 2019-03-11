58d10e97be5ace99816a4c7ec11cf0d98be7e06c
Zishan Ali <zishanali06@gmail.com>
2019-03-10 19:44:38 -0500

    done with step 4 on part 1

--- oop.js
+++ oop.js
@@ -38,3 +38,26 @@ person2.sayHello();
 person3.sayHello();
 person4.sayHello();
 person5.sayHello();
+
+function Person (name, city, age) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+}
+
+Person.prototype.sayHey = function () {
+    console.log(`Hey! My name is ${this.name} I am ${this.age} years old and live in ${this.city}`);
+}
+
+let p11 = new Person ('Zishan', 'Chicago', '29');
+let p22 = new Person ('Ashley', 'Dallas', '28');
+let p33 = new Person ('Fehzan', 'Boston', '29');
+let p44 = new Person ('Starla', 'Addison', '2');
+let p55 = new Person ('James', 'Chicago', '101');
+
+p11.sayHey();
+p22.sayHey();
+p33.sayHey();
+p44.sayHey();
+p55.sayHey();
+
