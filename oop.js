753aa75ec627bcf5dc334a76af2a17b20962424a
Zishan Ali <zishanali06@gmail.com>
2019-03-10 19:33:36 -0500

    step 1 done

---
+++ oop.js
@@ -0,0 +1,40 @@
+let person1 = {
+    name: 'Zishan',
+    sayHello: function () {
+        console.log(`Hello! My name is ${this.name}.`);
+    }
+};
+
+let person2 = {
+    name: 'Ashley',
+    sayHello: function () {
+        console.log(`Hello! My name is ${this.name}.`);
+    }
+};
+
+let person3 = {
+    name: 'Fehzan',
+    sayHello: function () {
+        console.log(`Hello! My name is ${this.name}.`);
+    }
+};
+
+let person4 = {
+    name: 'James',
+    sayHello: function () {
+        console.log(`Hello! My name is ${this.name}.`);
+    }
+};
+
+let person5 = {
+    name: 'Starla',
+    sayHello: function () {
+        console.log(`Hello! My name is ${this.name}.`);
+    }
+};
+
+person1.sayHello();
+person2.sayHello();
+person3.sayHello();
+person4.sayHello();
+person5.sayHello();
